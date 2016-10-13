7120fd4d9c6cd5652fdf35edbd4abeb2282319f2
yuw39 <project.yuw@gmail.com>
2016-10-14 02:12:19 +0900

    first commit.

--- WKWebViewSample/ViewController.swift
+++ WKWebViewSample/ViewController.swift
@@ -7,17 +7,40 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {
 
+    @IBOutlet var ibWKWebView: WKWebView?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        self.ibWKWebView = WKWebView()
+        self.ibWKWebView!.navigationDelegate = self
+        self.ibWKWebView!.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(self.ibWKWebView!)
+        
+        var viewBindingsDict = [String: AnyObject]()
+        viewBindingsDict["webView"] = self.ibWKWebView
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[webView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewBindingsDict))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[webView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: viewBindingsDict))
+        
+        let url = NSURL(string: "http://project-unknown.hatenablog.com/entry/2015/04/18/201907")
+        let request = NSURLRequest(url: url! as URL)
+        
+        self.ibWKWebView!.load(request as URLRequest)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
