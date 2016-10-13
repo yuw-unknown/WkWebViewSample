cdda1f471cdc6ff3c6c4437bb2f22932abe6fc9e
yuw39 <project.yuw@gmail.com>
2016-10-14 02:00:36 +0900

    Initial Commit

---
+++ WKWebViewSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKWebViewSample
+//
+//  Created by yuw on 2016/10/14.
+//  Copyright © 2016年 ProjectUnknown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
